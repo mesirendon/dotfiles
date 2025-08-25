b96c57079435f5ba1c5be4ff406871f82708c221
Mesi Rendon <hello@mesirendon.com>
2025-08-24 21:01:21 -0500

    Adding installing tmux plugins correctly

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 TPM_DIR="$HOME/.tmux/plugins/tpm"
 
 # Installing TPM if missing
-if [[ -d "$TPM_DIR" ]]; then
+if [[ ! -d "$TPM_DIR" ]]; then
 	echo "===> 💿 Installing Tmux Plugin Manager (TPM)"
 	git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
 else
@@ -15,8 +15,9 @@ fi
 if command -v tmux >/dev/null; then
 	echo "===> 💿 Installing tmux plugins via TPM"
 	tmux start-server
-	tmux new-session -d "exit" || true
+	tmux new-session -d -s __tpm_bootstrap 'sleep 0.1' || true
 	"$TPM_DIR/bin/install_plugins"
+	tmux kill-session -t __tpm_bootstrap >/dev/null 2>&1 || true
 	echo "✅ tmux plugins installed"
 else
 	echo "⚠️  tmux is not installed, skipping plugin setup"
