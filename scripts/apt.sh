7908308482a6d002bdcfb392754285c4fff4ca01
Mesi Rendon <mesi@modak.live>
2025-09-03 20:31:03 -0500

    Fix

--- scripts/apt.sh
+++ scripts/apt.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 sudo apt update
-xargs -a "$SCRIPT_DIR/Aptfile" sudo apt install -y
+xargs -a "$SCRIPT_DIR/../Aptfile" sudo apt install -y
