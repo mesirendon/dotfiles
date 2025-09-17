a9e52e272094ac1e0a1518cea0689c98c1c7ac16
Mesi Rendon <hello@mesirendon.com>
2025-09-16 20:00:42 -0500

    Organizing mason

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -205,7 +205,7 @@ map("n", "<leader>opr", "<cmd>Octo review start<cr>", { desc = "Review: Start" }
 map("n", "<leader>ops", "<cmd>Octo review submit<cr>", { desc = "Review: Submit" })
 map("n", "<leader>opb", "<cmd>Octo pr browser<cr>", { desc = "Open in Browser" })
 
-map("n", "<leader>oa", ":Octo assignee add @me<cr>", { desc = "PR: Assign me" })
+map("n", "<leader>oa", ":Octo assignee add mesirendon<cr>", { desc = "PR: Assign me" })
 map("n", "<leader>or", ":Octo reviewer add ", { desc = "PR: Add Reviewe(s)" })
 
 -- Issues
