866f06b0267879a67dc9c1a2d358c602ed3beab1
Mesi Rendon <hello@mesirendon.com>
2025-10-21 08:08:24 -0500

    Support for image?
    Signed-off-by: Mesi Rendon <hello@mesirendon.com>

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
     { import = "config.extras" },
     { import = "plugins" },
   },
+  rocks = { hererocks = true },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
     -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
--- nvim/.config/nvim/lua/plugins/image.lua
+++ nvim/.config/nvim/lua/plugins/image.lua
@@ -3,7 +3,7 @@ return {
   enabled = true,
   opts = {
     backend = "kitty", -- or "ueberzug" or "sixel"
-    processor = "magick_cli", -- or "magick_rock"
+    processor = "magick_rock", -- or "magick_rock"
     integrations = {
       markdown = {
         enabled = true,
@@ -14,13 +14,13 @@ return {
         floating_windows = false, -- if true, images will be rendered in floating markdown windows
         filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
         resolve_image_path = function(document_path, image_path, fallback)
-        -- document_path is the path to the file that contains the image
-        -- image_path is the potentially relative path to the image. for
-        -- markdown it's `![](this text)`
+          -- document_path is the path to the file that contains the image
+          -- image_path is the potentially relative path to the image. for
+          -- markdown it's `![](this text)`
 
-        -- you can call the fallback function to get the default behavior
-        return fallback(document_path, image_path)
-      end,
+          -- you can call the fallback function to get the default behavior
+          return fallback(document_path, image_path)
+        end,
       },
       neorg = {
         enabled = true,
