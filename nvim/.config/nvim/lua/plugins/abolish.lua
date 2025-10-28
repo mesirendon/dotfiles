02e31de752649a5e7a830ad5242f995fb996c05a
Mesi Rendon <hello@mesirendon.com>
2025-10-28 14:10:32 -0500

    Adding cases changer
    Signed-off-by: Mesi Rendon <hello@mesirendon.com>

---
+++ nvim/.config/nvim/lua/plugins/abolish.lua
@@ -0,0 +1,11 @@
+return {
+  "tpope/vim-abolish",
+  keys = {
+    { "crs", desc = "snake_case" },
+    { "crc", desc = "camelCase" },
+    { "crm", desc = "PascalCase" },
+    { "cru", desc = "UPPER_SNAKE_CASE" },
+    { "cr-", desc = "kebab-case" },
+    { "crt", desc = "Title Case" },
+  },
+}
