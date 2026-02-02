f343451e6d47460634a37d58762682ca02564200
Mesi Rendon <hello@mesirendon.com>
2026-02-02 16:15:57 -0500

    PlantUML Fix (#38)
    This change looks to use the installed PlantUML
    from brew installation and looks for the prefix
    depending on the operating system.
    
    Signed-off-by: Mesi Rendon <hello@mesirendon.com>

--- nvim/.config/nvim/lua/plugins/plantuml.lua
+++ nvim/.config/nvim/lua/plugins/plantuml.lua
@@ -1,3 +1,19 @@
+local function file_exists(p)
+  return p and vim.uv.fs_stat(p) ~= nil
+end
+local function get_plantuml_jar()
+  -- Using brew prefix (works for Homebrew + Linuxbrew)
+  if vim.fn.executable("brew") == 1 then
+    local prefix = vim.fn.system({ "brew", "--prefix", "plantuml" }):gsub("%s+$", "")
+    if prefix ~= "" then
+      local jar = prefix .. "/libexec/plantuml.jar"
+      if file_exists(jar) then
+        return jar
+      end
+    end
+  end
+end
+
 return {
   { "aklt/plantuml-syntax", ft = { "plantuml" } },
   { "tyru/open-browser.vim", lazy = true },
@@ -11,6 +27,11 @@ return {
           puml = "plantuml",
         },
       })
+
+      local jar = get_plantuml_jar()
+      if jar then
+        vim.g["plantuml_previewer#plantuml_jar_path"] = jar
+      end
     end,
   },
 }
